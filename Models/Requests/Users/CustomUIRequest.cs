6ba4c1c0201a85250716647feb38f9eb9d9b5727
karina-y <karinayeznaian@yahoo.com>
2016-08-10 17:37:34 -0700

    Create CustomUIRequest.cs

---
+++ Models/Requests/Users/CustomUIRequest.cs
@@ -0,0 +1,9 @@
+namespace RapidRents.Web.Models.Requests.Users
+{
+    public class CustomUIRequest
+    {
+        public int CustomizationId { get; set; }
+
+        public string Value { get; set; }
+    }
+}
