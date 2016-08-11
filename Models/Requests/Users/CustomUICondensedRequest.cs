745e754be90990f98c7e55e64bbb74612cdd6b05
karina-y <karinayeznaian@yahoo.com>
2016-08-10 17:37:19 -0700

    Create CustomUICondensedRequest.cs

---
+++ Models/Requests/Users/CustomUICondensedRequest.cs
@@ -0,0 +1,9 @@
+namespace RapidRents.Web.Models.Requests.Users
+{
+    public class CustomUICondensedRequest
+    {
+        public string JSName { get; set; }
+
+        public string Value { get; set; }
+    }
+}
